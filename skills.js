1f111ac8a61184659f0713eebff7a0dd8185fb03
José Díaz <me@josediaz.dev>
2023-11-02 19:20:45 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,19 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+function multiplyNumbers(var1, var2) {
+    return var1 * var2;
+}
+
+function divideNumbers(var1, var2) {
+    return var1 / var2;
+}
+
+function subtractNumbers(var1, var2) {
+    return var1 - var2;
+}
+
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
